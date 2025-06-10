a7166893f51b71539652b8c009be05f8a43d99d4
Asmae LAHROUB <135254413+LahroubAsmae@users.noreply.github.com>
2025-06-10 14:13:49 +0100

    Intégration des services : user, auth, log

---
+++ src/test/java/com/lahroubasmae/user_management_microservices/UserManagementMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahroubasmae.user_management_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
